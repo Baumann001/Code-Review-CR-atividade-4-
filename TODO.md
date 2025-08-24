f574c3dc5742f3bacf7f9f65b3d6f2bdec710e8a
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-08-24 16:42:44 -0300

    add-

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# Fixes Needed for CRUD System
+
+## High Priority Fixes:
+- [ ] Fix SQL syntax error in create.php (VALUE → VALUES)
+- [ ] Fix typo in success message (criadocom → criado com)
+- [ ] Implement SQL injection protection using prepared statements
+
+## Security Improvements:
+- [ ] Replace direct variable interpolation with prepared statements
+- [ ] Add input validation
+
+## Files to Edit:
+- create.php (main fixes)
--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "crud_system";
+$servername = 'localhost';
+$username ='root';
+$password = 'root';
+$dbname = 'crud_system';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if($conn ->connect_error){
-    die("Conexão falhou:" . $conn ->connect_error);
+if ($conn->connect_error){
+    die("Conexão falhou:" . $conn->connect_error);
 }
