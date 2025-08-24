fdfacfc9d05cc1c125a0a02c40b2097986a50e00
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-08-24 17:09:39 -0300

    add-

--- update.php
+++ update.php
@@ -22,3 +22,31 @@ if ($conn -> query($sql) === true){
 
 
 ?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form method ="POST"  action="update.php?id=<?php echo $row['id'];?>">
+
+    <label for="name">Nome:</label>
+    <input type="text" name= "name"  value = "<?php echo $row['name'];?>"required>
+
+     <label for="email">Email:</label>
+    <input type="email" name= "email"  value = "<?php echo $row['email'];?>"required>
+
+    <input type="submit" value = "Atualizar">
+
+    </form>
+
+    
+        
+    
+    
+</body>
+</html>
